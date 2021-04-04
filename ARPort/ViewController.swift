c284a341f0732385451a4c2a39b5bb3e0c3910cf
Sascha Sallès <sascha.salles@ynov.com>
2021-04-04 17:42:39 +0200

    Add SceneKit Protocol conformance to ViewController, add helper methods

--- ARPort/ViewController.swift
+++ ARPort/ViewController.swift
@@ -10,13 +10,21 @@ import ARKit
 
 // MARK: - App State Management
 
+enum AppState: Int16 {
+  case detectSurface
+  case pointAtSurface
+  case tapToStart
+  case started
+}
 
 // MARK: - UIViewController
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
   // MARK: - Properties
-
+  var trackingStatus: String = ""
+  var statusMessage: String  = ""
+  var appState: AppState = .detectSurface
 
   // MARK: - IB Outlets
   @IBOutlet var sceneView: ARSCNView!
@@ -25,6 +33,7 @@ class ViewController: UIViewController {
 
   // MARK: - IB Actions
   @IBAction func resetButtonPressed(_ sender: Any) {
+    
   }
 
   @IBAction func tapGestureHandler(_ sender: Any) {
@@ -32,6 +41,7 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    self.initScene()
   }
 
   override func viewWillAppear(_ animated: Bool) {
@@ -56,8 +66,18 @@ class ViewController: UIViewController {
 
 // MARK: - App Management
 extension ViewController {
+  func startApp() {
+    DispatchQueue.main.async {
+      self.appState = .detectSurface
+    }
+  }
 
-  // Add code here...
+  func resetApp() {
+    DispatchQueue.main.async {
+      //self.resetARSession()
+      self.appState = .detectSurface
+    }
+  }
 
 }
 
@@ -77,8 +97,32 @@ extension ViewController {
 
 // MARK: - Scene Management
 extension ViewController {
+  func initScene() {
+    let scene = SCNScene()
+    sceneView.scene = scene
+    sceneView.delegate = self
+  }
 
-  // Add code here...
+  func updateStatus() {
+    switch appState {
+    case .detectSurface:
+      statusMessage = "Scan available flat surfaces..."
+    case .pointAtSurface:
+      statusMessage = "Point at designated surface first!"
+    case .tapToStart:
+      statusMessage = "Tap to start."
+    case .started:
+      statusMessage = "Tap objects for more info"
+    }
+
+    self.statusLabel.text = trackingStatus != "" ? "\(trackingStatus)" : "\(statusMessage)"
+  }
+
+  func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+    DispatchQueue.main.async {
+      self.updateStatus()
+    }
+  }
 
 }
 
