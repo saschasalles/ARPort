a67360b3ec5fe121cd87240a791c7ece8133ea7c
Sascha Sallès <sascha.salles@ynov.com>
2021-04-04 17:23:57 +0200

    Init clean project, add assets, delete generated code

--- ARPort/ViewController.swift
+++ ARPort/ViewController.swift
@@ -4,71 +4,87 @@
 //
 //  Created by Sascha Sallès on 04/04/2021.
 //
-
 import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+// MARK: - App State Management
 
-    @IBOutlet var sceneView: ARSCNView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
 
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
-    }
+// MARK: - UIViewController
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
-    }
-*/
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
-    }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
+class ViewController: UIViewController {
+
+  // MARK: - Properties
+
+
+  // MARK: - IB Outlets
+  @IBOutlet var sceneView: ARSCNView!
+  @IBOutlet weak var statusLabel: UILabel!
+  @IBOutlet weak var resetButton: UIButton!
+
+  // MARK: - IB Actions
+  @IBAction func resetButtonPressed(_ sender: Any) {
+  }
+
+  @IBAction func tapGestureHandler(_ sender: Any) {
+  }
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    print("*** ViewWillAppear()")
+  }
+
+  override func viewWillDisappear(_ animated: Bool) {
+    super.viewWillDisappear(animated)
+    print("*** ViewWillDisappear()")
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    print("*** DidReceiveMemoryWarning()")
+  }
+
+  override var prefersStatusBarHidden: Bool {
+    return true
+  }
+}
+
+// MARK: - App Management
+extension ViewController {
+
+  // Add code here...
+
+}
+
+// MARK: - AR Coaching Overlay
+extension ViewController {
+
+  // Add code here...
+
+}
+
+// MARK: - AR Session Management (ARSCNViewDelegate)
+extension ViewController {
+
+  // Add code here...
+
+}
+
+// MARK: - Scene Management
+extension ViewController {
+
+  // Add code here...
+
+}
+
+// MARK: - Focus Node Management
+extension ViewController {
+
+  // Add code here...
+
 }
